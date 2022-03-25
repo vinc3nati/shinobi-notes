8287fa4a8806a3317b784b223027e5ad72332431
Vinit Kanse <vinitkansend@gmail.com>
2022-03-25 16:00:57 +0530

    Notes Template

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Mockman from "mockman-js";
 import "./App.css";
-import logo from "./logo.png";
+import { Home } from "./pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/mock" element={<Mockman />} />
+    </Routes>
   );
 }
 
