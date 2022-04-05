710cd6f8bb9485b1558622193986c534515df7f2
Vinit Kanse <vinitkansend@gmail.com>
2022-04-05 16:31:43 +0530

    Feat: Added Error Page

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <section id="error-page">
+      <div className="error-container">
+        <div className="error-symbol"></div>
+        <div className="error-content">
+          <div className="error-heading">
+            <p>Error</p>
+            <p>404</p>
+          </div>
+          <div className="error-text">Page not found</div>
+          <div className="error-button-grp">
+            <button
+              className="btn outline-warning"
+              onClick={() => navigate("/")}
+            >
+              Home
+            </button>
+            <button className="btn warning" onClick={() => navigate("/notes")}>
+              Notes
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
