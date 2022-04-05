f3a8084525caa55812e5ae7e0cf2b8d438aaf437
Vinit Kanse <vinitkansend@gmail.com>
2022-04-05 22:30:10 +0530

    Fix: added user img

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth } from "../../contexts";
+import UserImage from "../../assets/images/dummy.jpg";
 
 const convertToProperDate = (date) => {
   const localDate = new Date(date);
@@ -17,23 +18,28 @@ export const Profile = () => {
   } = useAuth();
   return (
     <section id="profile">
-      <div className="profile-section">
-        <span className="profile-title">Name :</span>
-        <span className="profile-value">{user.name}</span>
+      <div className="user-img-container">
+        <img className="img img-round" src={UserImage} alt="user-profile" />
       </div>
-      <div className="profile-section">
-        <span className="profile-title">Email :</span>
-        <span className="profile-value">{user.email}</span>
+      <div className="profile-content">
+        <div className="profile-section">
+          <span className="profile-title">Name :</span>
+          <span className="profile-value">{user.name}</span>
+        </div>
+        <div className="profile-section">
+          <span className="profile-title">Email :</span>
+          <span className="profile-value">{user.email}</span>
+        </div>
+        <div className="profile-section">
+          <span className="profile-title">Joined At :</span>
+          <span className="profile-value">
+            {convertToProperDate(user.createdAt)}
+          </span>
+        </div>
+        <button className="btn outline-warning" onClick={handleLogOut}>
+          Logout
+        </button>
       </div>
-      <div className="profile-section">
-        <span className="profile-title">Joined At :</span>
-        <span className="profile-value">
-          {convertToProperDate(user.createdAt)}
-        </span>
-      </div>
-      <button className="btn outline-warning" onClick={handleLogOut}>
-        Logout
-      </button>
     </section>
   );
 };
